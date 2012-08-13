c94083a87b2bec1dcf6b3a6b44b05723ee7a4adb
Aaron Shafovaloff <aaronshaf@gmail.com>
2012-08-12 19:16:44 -0600

    Initial development

--- ga-experiments.js
+++ ga-experiments.js
@@ -1,5 +1,19 @@
 GAExperiments = {
+	this.experiments = [];
+	
 	this.add = function(options) {
-		
+		if(typeof $ === 'undefined'
+				|| typeof _gaq === 'undefined'
+				|| !window.localStorage
+				|| !options.samples) return;
+		var id = localStorage.getItem('ga-experiments-id');
+		if(!id) {
+			id = Math.random();
+			localStorage.setItem('ga-exeriments-id',id);
+		}
+		var sampleKeys = Object.keys(options.samples);
+		options.samples[sampleKeys[Math.floor(id * (sampleKeys.length)) + 1]]();
 	};
 };
+
+//_gaq.push(["_setCustomVar", options.slot, options.name, value, 1]);
